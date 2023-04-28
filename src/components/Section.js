2eb1154cc2f3324f2032b81df620c39bd82139c1
AnanDEswaran18 <bagavathianandhan002@gmail.com>
2023-04-28 11:13:12 +0530

    downArrow

--- src/components/Section.js
+++ src/components/Section.js
@@ -8,14 +8,17 @@ function Section() {
             <h1>Model S</h1>
             <p>Order Online for Touchless delivery</p>
         </ItemText>
-        <ButtonGroup>
-            <LeftButton>
-                Custom Order
-            </LeftButton>
-            <RightButton>
-                Existing Inventory 
-            </RightButton>
-        </ButtonGroup>
+        <Buttons>
+            <ButtonGroup>
+                <LeftButton>
+                    Custom Order
+                </LeftButton>
+                <RightButton>
+                    Existing Inventory 
+                </RightButton>
+            </ButtonGroup>
+            <DownArrow src="/images/down-arrow.svg"/>
+        </Buttons>
     </Wrap>
   )
 }
@@ -25,20 +28,24 @@ const Wrap = styled.div`
     width:100vw;
     height:100vh;
     background-size:cover;
-    background-position:centre;
+    background-position:center;
     background-repeat:no-repeat;
     background-image:url('/images/model-s.jpg');
     display:flex;
     flex-direction:column; 
     justify-content:space-between;
+    align-items:center;
 `
 const ItemText = styled.div`
     padding-top:15vh;
     text-align:center;
 `//component
 
+const Buttons = styled.div``;
+
 const ButtonGroup = styled.div`
     display:flex;
+    margin-bottom:30px;
 `;
 
 const LeftButton = styled.div`
@@ -53,8 +60,15 @@ const LeftButton = styled.div`
     opacity:0.85;
     text-transform:uppercase;
     font-size:12px;
+    cursor:pointer;
 `;
 
 const RightButton = styled(LeftButton)`
 
+`;
+
+const DownArrow = styled.img`
+    margin-top:20px;
+    height:40px;
+    cursor:pointer;
 `;
