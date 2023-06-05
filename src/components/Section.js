af51ef3936fa1255c7b55272c03b2c3dc9868658
Anand Eswaran <100366969+AnanDEswaran18@users.noreply.github.com>
2023-06-05 21:02:58 +0530

    Update Section.js

--- src/components/Section.js
+++ src/components/Section.js
@@ -43,7 +43,7 @@ const Container = styled.div`
     flex-direction:column; 
     justify-content:space-between;
     align-items:center;
-    background-image: ${props=>`url("/images/${props.bgImage}")`}
+    background-image: ${props=>`url("images/${props.bgImage}")`}
 `
 const ItemText = styled.div`
     margin-top:15vh;
